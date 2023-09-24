2d7610576a78d47d00dc0c52a03e078b8fa3985b
Your Name <you@example.com>
2023-09-24 16:47:48 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function toggleMode(){
+    const html = document.documentElement;
+    html.classList.toggle('light')
+    
+    // pegar a tag img
+    const img = document.querySelector('#profile img')
+    
+
+    // substituir a imagem
+    if(html.classList.contains('light')) {
+        // se tiver light mode, adicionar a imagem light
+        img.setAttribute('src','./assets/avatar-light.png')
+        img.setAttribute('alt',"Minha foto light mode")
+    } else {
+        // se tiver sem light mode, adicionar a imagem light
+       img.setAttribute('src','./assets/avatar.png')
+    }
+
+
+    // TERNARIO para o toggle
+    
+    // html.classList.contains('light') 
+    // ? html.classList.remove('light') 
+    // : html.classList.add('light')
+
+
+
+
+
+}
