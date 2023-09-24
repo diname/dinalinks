3d62c2c6aae233ac14af8ae69ac97ae082358782
Your Name <you@example.com>
2023-09-24 17:08:59 -0300

    Alterações de layout e inserção do readme

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ function toggleMode(){
     // substituir a imagem
     if(html.classList.contains('light')) {
         // se tiver light mode, adicionar a imagem light
-        img.setAttribute('src','./assets/avatar-light.png')
+        img.setAttribute('src','./assets/lucas-medina-light.png')
         img.setAttribute('alt',"Minha foto light mode")
     } else {
         // se tiver sem light mode, adicionar a imagem light
-       img.setAttribute('src','./assets/avatar.png')
+       img.setAttribute('src','./assets/lucas-medina.jpeg')
     }
 
 
